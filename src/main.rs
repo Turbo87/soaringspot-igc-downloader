84b6bd2cf347c306ad3cd89c1a878a3dd4cc16fd
Tobias Bieniek <tobias@bieniek.cloud>
2025-06-23 09:47:51 +0200

    Add support for `--output` argument

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod parser;
 
 use clap::Parser;
 use parser::parse_igc_files;
+use std::path::PathBuf;
 use url::Url;
 
 #[derive(Parser)]
@@ -11,6 +12,10 @@ use url::Url;
 struct Args {
     /// SoaringSpot URL to download from
     url: String,
+
+    /// Output directory for IGC files (defaults to current directory)
+    #[arg(short, long)]
+    output: Option<PathBuf>,
 }
 
 #[tokio::main]
@@ -30,6 +35,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         // Parse HTML and extract IGC file information
         let igc_files = parse_igc_files(&html)?;
         println!("Found {} IGC files:", igc_files.len());
+
+        // Determine output directory
+        let output_dir = args.output.unwrap_or_else(|| PathBuf::from("."));
+        println!("Output directory: {}", output_dir.display());
 
         for igc_file in &igc_files {
             println!("  {} -> {}", igc_file.callsign, igc_file.download_url);
